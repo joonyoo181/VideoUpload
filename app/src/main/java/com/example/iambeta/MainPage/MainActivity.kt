14d1558f6133acaae9f7237d319fbaae86471997
joonyoo181 <joon.yoo181@gmail.com>
2020-03-12 11:10:41 -0700

    New Main Page Layout

--- app/src/main/java/com/example/iambeta/MainPage/MainActivity.kt
+++ app/src/main/java/com/example/iambeta/MainPage/MainActivity.kt
@@ -4,42 +4,40 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.iambeta.FAQPage.FAQPage
 import com.example.iambeta.ProfilePage.ProfilePage
 import com.example.iambeta.R
 
 class MainActivity : AppCompatActivity() {
 
     //Declaring Button Variables
-    private var mainToProfilePage: Button? = null
-    private var mainToFAQPage: Button? = null
+    var mainToChatPage: Button? = null
+    var mainVideoComments: Button? = null
+    var mainToDiscoverPage: Button? = null
+    var mainToProfilePage: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Initializing mainToChatPage Button
+        mainToChatPage = findViewById(R.id.Button_mainToChatPage)
+
+        //Initializing mainVideoComments Button
+        mainVideoComments = findViewById(R.id.Button_mainVideoComments)
+
+        //Initializing mainToDiscoverPage Button
+        mainToDiscoverPage = findViewById(R.id.Button_mainToDiscoverPage)
 
         //Initializing mainToProfilePage Button & Checking if pressed
         mainToProfilePage = findViewById(R.id.Button_mainToProfilePage)
         mainToProfilePage!!.setOnClickListener{
             openProfilePage()
         }
-
-        //Initializing mainToFAQPage Button & Checking if pressed
-        mainToFAQPage = findViewById(R.id.Button_mainToFAQPage)
-        mainToFAQPage!!.setOnClickListener{
-            openFAQPage()
-        }
     }
 
-    //Opening Profile Page From Main Page
+    //Opening Profile Page from Main Page
     private fun openProfilePage(){
         val intent = Intent(this, ProfilePage::class.java)
-        startActivity(intent)
-    }
-
-    //Opening FAQ Page From Main Page
-    private fun openFAQPage(){
-        val intent = Intent(this, FAQPage::class.java)
         startActivity(intent)
     }
 }
--- app/src/main/java/com/example/iambeta/ProfilePage/ProfilePage.kt
+++ app/src/main/java/com/example/iambeta/ProfilePage/ProfilePage.kt
@@ -4,22 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.iambeta.FAQPage.FAQPage
 import com.example.iambeta.MainPage.MainActivity
 import com.example.iambeta.R
 
 class ProfilePage : AppCompatActivity() {
 
     //Declaring Button Variables
-    private var profileToEditPage: Button? = null
     private var profileToMainPage: Button? = null
     private var profileToLogOut: Button? = null
+    private var profileToEditPage: Button? = null
+    private var profileToFAQPage: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_page)
-
-        //Initializing profileEditPage Button
-        profileToEditPage = findViewById(R.id.Button_profileToEditPage)
 
         //Initializing profileToMainPage Button & Checking if pressed
         profileToMainPage = findViewById(R.id.Button_profileToMainPage)
@@ -29,10 +28,26 @@ class ProfilePage : AppCompatActivity() {
 
         //Initializing profileLogOut Button
         profileToLogOut = findViewById(R.id.Button_profileLogOut)
+
+        //Initializing profileEditPage Button
+        profileToEditPage = findViewById(R.id.Button_profileToEditPage)
+
+        //Initializing profileToFAQPage Button & Checking if pressed
+        profileToFAQPage = findViewById(R.id.Button_profileToFAQPage)
+        profileToFAQPage!!.setOnClickListener {
+            openFAQPage()
+        }
     }
 
+    //Opening Main Page from Profile Page
     private fun openMainPage(){
         val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    //Opening FAQ Page from Profile Page
+    private fun openFAQPage(){
+        val intent = Intent(this, FAQPage::class.java)
         startActivity(intent)
     }
 }
