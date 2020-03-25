e963a1c0d3b1c1ece6011324ff395e75f5eb0318
genelikespie <genenakamura@gmail.com>
2020-03-25 13:23:03 -0700

    Adding additional changes for sign up and sign in with email + setup draft for google

---
+++ app/src/main/java/com/example/iambeta/authentication/SignInActivity.kt
@@ -0,0 +1,51 @@
+package com.example.iambeta.authentication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.iambeta.R
+import com.example.iambeta.mainPage.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_signin.*
+
+
+class SignInActivity : AppCompatActivity() {
+
+    var mAuth: FirebaseAuth? = null
+    var mAuthListener: FirebaseAuth.AuthStateListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+
+        mAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener {  }
+
+    }
+
+    fun signIn(view: View) {
+        mAuth!!.signInWithEmailAndPassword(emailText.text.toString(), passwordText.text.toString())
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+    }
+
+    fun signUp(view: View) {
+        mAuth!!.createUserWithEmailAndPassword(emailText.text.toString(), passwordText.text.toString())
+            .addOnCompleteListener{ task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext,"User Created", Toast.LENGTH_LONG).show()
+                    //intent
+                }
+            }.addOnFailureListener { exception ->
+                    Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+    }
+}
